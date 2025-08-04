2b5931003481e040ae96bc359b0a29bc938fecd4
TTMaheepala <it23729902@my.sliit.lk>
2025-08-04 09:49:54 +0530

    Update App.jsx
    update in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,16 @@
-
-import React from 'react'
-import Home from './Components/Home/Home';
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 import Login from './Components/UserManage/Login';
 import RegisterOfficer from './Components/admin/RegisterOfficer';
-import AdminDashboard from  './Components/admin/AdminDashboard';
-import Home from './Components/Home/Home'; // <-- Add this import
-
-
-
+import AdminDashboard from './Components/admin/AdminDashboard';
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
-  )
-}
-
-export default App;
-
-
     <Router>
       <Routes>
-
         {/* Home Route */}
-        <Route path="/" element={<Home />} /> {/* <-- Add this line */}
+        <Route path="/" element={<Home />} />
 
         {/* Public Routes */}
         <Route path="/login" element={<Login />} />
@@ -35,7 +18,6 @@ export default App;
         {/* Admin Routes */}
         <Route path="/Admin/register-officer" element={<RegisterOfficer />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
 
         {/* Add more routes below */}
         {/* Example: */}
