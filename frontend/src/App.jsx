dcaff570dcd48b3b68313e4e0b79aa480176e427
TTMaheepala <it23729902@my.sliit.lk>
2025-08-04 08:57:23 +0530

    Home.jsx
    create home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Home from './Components/Home/Home';
 
 const App = () => {
   return (
     <div>
-      
+      <Home></Home>
     </div>
   )
 }
 
-export default App
+export default App;
+
