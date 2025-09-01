2f4da4ef6e1d462fda0e3ff725516bbb1ec8a81f
Ashen_IT21286278 <enuriillesinghe@gmail.com>
2025-09-01 09:03:55 +0530

    Accidentform  and backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 
 const officerRoutes = require('./routes/officerRoutes');
 const authRoutes = require('./routes/authRoutes');
+const accidentRoutes = require('./routes/accidentRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,12 +13,15 @@ connectDB();
 const app = express();
 
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
-app.use(cors({
-  origin: CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
+app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
@@ -26,10 +30,16 @@ app.get('/api/health', (_req, res) => res.json({ ok: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/officers', officerRoutes);
 
+//Enuri Routes
+
+app.use('/api/accidents', accidentRoutes);
+app.get('/', (_req, res) => res.send('Police360 API running'));
+
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 
 app.use((err, _req, res, _next) => {
-  if (err.type === 'entity.too.large') return res.status(413).json({ message: 'Payload too large' });
+  if (err.type === 'entity.too.large')
+    return res.status(413).json({ message: 'Payload too large' });
   res.status(500).json({ message: 'Server error' });
 });
 
