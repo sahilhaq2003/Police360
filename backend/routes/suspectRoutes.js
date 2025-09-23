515ec45f89611b1b0f4090a6913a4f9ca28c42b7
m.@G <207574628+ThisaraDenu@users.noreply.github.com>
2025-09-23 14:23:31 +0530

    sg

--- backend/routes/suspectRoutes.js
+++ backend/routes/suspectRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { getAllSuspects, addSuspect, getSuspectById, updateSuspect, deleteSuspect } = require('../controllers/SuspectController');
 const { protect } = require('../middlewares/authMiddleware');
 
-router.route('/').get(protect, getAllSuspects).post(protect, addSuspect);
-router.route('/:id').get(protect, getSuspectById).put(protect, updateSuspect).delete(protect, deleteSuspect);
+router.get('/', protect, SuspectController.getAllSuspects);
+router.post('/', protect, SuspectController.addSuspect);
+router.get('/:id', protect, SuspectController.getSuspectById);
+router.put('/:id', protect, SuspectController.updateSuspect);
+router.delete('/:id', protect, SuspectController.deleteSuspect);
 
 module.exports = router;
+
